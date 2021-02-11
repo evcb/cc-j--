c1c2f6b7d32a47f45eaab75d4a778560afac6d87
Elvis Camilo <s190395@student.dtu.dk>
2021-02-11 04:53:27 -0300

    Added DIV

--- tests/junit/JMinusMinusTestRunner.java
+++ tests/junit/JMinusMinusTestRunner.java
@@ -22,6 +22,7 @@ public class JMinusMinusTestRunner {
         suite.addTestSuite(SeriesTest.class);
         suite.addTestSuite(ClassesTest.class);
         suite.addTestSuite(DivisionTest.class);
+        suite.addTestSuite(RemainderTest.class);
         return suite;
     }
 
---
+++ tests/junit/RemainderTest.java
@@ -0,0 +1,23 @@
+package junit;
+
+import junit.framework.TestCase;
+import pass.Remainder;
+
+public class RemainderTest extends TestCase {
+    private Remainder remainder;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        remainder = new Remainder();
+    }
+
+    protected void tearDown() throws Exception { 
+        super.tearDown();
+    }
+
+    public void testRemainder () {
+        this.assertEquals(remainder.remainder(0, 42), 0); 
+        this.assertEquals(remainder.remainder(42, 1), 0);
+        this.assertEquals(remainder.remainder(127, 3), 1);
+    }
+}
