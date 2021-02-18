2e3cb609afc1aa5af84bd4800bc28a5c4145d9db
WilliamWulff <s174880@student.dtu.dk>
2021-02-18 09:19:49 +0100

    Fixed test cases for Unsigned Right Shift

--- tests/junit/JMinusMinusTestRunner.java
+++ tests/junit/JMinusMinusTestRunner.java
@@ -23,7 +23,7 @@ public class JMinusMinusTestRunner {
         suite.addTestSuite(ClassesTest.class);
         suite.addTestSuite(DivisionTest.class);
         suite.addTestSuite(RemainderTest.class);
-	suite.addTestSuite(USignedRightShiftTest.class)
+	suite.addTestSuite(USignedRightShiftTest.class);
         return suite;
     }
 
