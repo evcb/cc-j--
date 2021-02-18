b2e92981292b4a13ebebe94813df3ddbe6918fac
Elvis Camilo <s190395@student.dtu.dk>
2021-02-18 15:39:19 +0100

    Added new tokens and multi-line comment

---
+++ tests/pass/Comment.java
@@ -0,0 +1,23 @@
+// Copyright 2013 Bill Campbell, Swami Iyer and Bahar Akbal-Delibas
+
+package pass;
+
+public class Comment {
+
+    /* comment1 */
+    /* 
+        comment2
+        comment3
+        comment4
+        comment5
+    */
+    public static String methodWithComment() {
+        // comment6 comment7 comment8
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Comment.methodWithComment();
+    }
+
+}
