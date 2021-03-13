337aaadcb243e203471ef92cca20b7cf4b72026c
Danial Virk <s193167@student.dtu.dk>
2021-03-13 12:13:05 +0100

    Added ConditionalExpression

---
+++ tests/pass/ConditionalExpression.java
@@ -0,0 +1,10 @@
+package pass;
+
+import jdk.internal.agent.resources.agent;
+import jdk.internal.agent.resources.agent_ja;
+
+public class ConditionalExpression {
+    public boolean express(boolean a) {
+        return a ? true : false;
+    }
+}
