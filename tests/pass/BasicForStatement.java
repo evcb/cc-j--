80661a46ebc76fadb362b56e153b8cfea449ca9d
WilliamWulff <s174880@student.dtu.dk>
2021-03-18 11:42:37 +0100

    Baseline working commit for for-statements with the JavaCC parser

--- tests/pass/BasicForStatement.java
+++ tests/pass/BasicForStatement.java
@@ -2,7 +2,7 @@ package pass;
 
 public class BasicForStatement {
     public int forCount(int x, int y) {
-	for (i = 0 ; y > i; i++, y--) {
+	for (int i = 0 ; y > i ; x--) {
 	//for ( ; ; ) {
 	    x = x*2;
 	}
